82afcea79d875a68ee4d80d2431868a8641e76c4
eecavanna <eecavanna@users.noreply.github.com>
2024-12-05 00:02:36 -0800

    Build consolidated workflow documentation Sphinx website via Dockerfile

---
+++ src/workflow_docs/conf.py
@@ -0,0 +1,28 @@
+# Note: The values in this file were copied from: https://github.com/microbiomedata/workflow_documentation/blob/master/docs/conf.py
+
+# The name of the _documented_ project.
+# Docs: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-project
+project = 'NMDC Workflows'
+
+# The author of the _documented_ project.
+# Docs: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-author
+author = 'National Microbiome Data Collaborative'
+
+# A copyright statement.
+# Docs: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-copyright
+copyright = '2024, National Microbiome Data Collaborative'
+
+# The documented project's _simplified_ and _full_ version identifiers.
+# Note: This chained assignment keeps them identical to one another.
+# TODO: Do team members expect these to be kept in sync with anything external?
+# Docs: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-version
+# Docs: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-release
+version = release = '0.1.0'
+
+# The source document containing the `toctree` for which we want Sphinx to build a document tree.
+# Docs: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-master_doc
+master_doc = 'index'
+
+# The theme for HTML output.
+# Docs: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-master_doc
+html_theme = 'sphinx_rtd_theme'
---
+++ src/workflow_docs/index.rst
@@ -0,0 +1,21 @@
+..
+    Note: This file was copied from: https://github.com/microbiomedata/workflow_documentation/blob/master/docs/index.rst
+    TODO: Fix the typo in "Assemly" below (change to "Assembly").
+
+Welcome to NMDC Workflows's documentation!
+==========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   chapters/overview.rst
+   chapters/1_RQC/index.rst
+   chapters/2_ReadAnalysis/index.rst
+   chapters/3_MetaGAssemly/index.rst
+   chapters/4_MetaGAnnotation/index.rst
+   chapters/5_MAG/index.rst
+   chapters/6_MetaT/index.rst
+   chapters/7_Metaproteomics/index.rst
+   chapters/8_Metabolomics/index.rst
+   chapters/9_NOM/index.rst
