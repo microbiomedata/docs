b3270ee845f1b06f7da980456878130479739074
eecavanna <134325062+eecavanna@users.noreply.github.com>
2024-12-09 22:57:15 -0800

    Fix typo in the word "Metatranscriptome"

--- src/workflow_docs/fetch_docs_sources.sh
+++ src/workflow_docs/fetch_docs_sources.sh
@@ -37,8 +37,8 @@ mv /tmp/clones/mg_annotation/docs     /tmp/book/src/chapters/5_Metagenome_and_Me
 mv /tmp/clones/metaMAGs/docs          /tmp/book/src/chapters/6_Metagenome_Assembled_Genome
 mv /tmp/clones/metaT/docs             /tmp/book/src/chapters/7_Metatranscriptome_Workflow_Overview
 mv /tmp/clones/metaT_ReadsQC/docs     /tmp/book/src/chapters/8_Metatranscriptome_Reads_QC
-mv /tmp/clones/metaT_Assembly/docs    /tmp/book/src/chapters/9_Metranscriptome_Assembly
-mv /tmp/clones/metaT_ReadCounts/docs  /tmp/book/src/chapters/10_Metranscriptome_Expression
+mv /tmp/clones/metaT_Assembly/docs    /tmp/book/src/chapters/9_Metatranscriptome_Assembly
+mv /tmp/clones/metaT_ReadCounts/docs  /tmp/book/src/chapters/10_Metatranscriptome_Expression
 mv /tmp/clones/metaPro/docs           /tmp/book/src/chapters/11_Metaproteomics
 mv /tmp/clones/metaMS/docs            /tmp/book/src/chapters/12_Metabolomics
 mv /tmp/clones/enviroMS/docs          /tmp/book/src/chapters/13_Natural_Organic_Matter
--- src/workflow_docs/index.rst
+++ src/workflow_docs/index.rst
@@ -23,8 +23,8 @@ Welcome to the NMDC Workflow documentation!
    chapters/6_Metagenome_Assembled_Genome/index.rst
    chapters/7_Metatranscriptome_Workflow_Overview/index.rst
    chapters/8_Metatranscriptome_Reads_QC/index.rst
-   chapters/9_Metranscriptome_Assembly/index.rst
-   chapters/10_Metranscriptome_Expression/index.rst
+   chapters/9_Metatranscriptome_Assembly/index.rst
+   chapters/10_Metatranscriptome_Expression/index.rst
    chapters/11_Metaproteomics/index.rst
    chapters/12_Metabolomics/index.rst
    chapters/13_Natural_Organic_Matter/index.rst
