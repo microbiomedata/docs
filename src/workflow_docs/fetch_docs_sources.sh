5070bb075faec18db0444a17c7ff3eca257d797e
eecavanna <134325062+eecavanna@users.noreply.github.com>
2024-12-09 22:54:34 -0800

    Add missing Git commands and synchronize file paths across files

--- src/workflow_docs/fetch_docs_sources.sh
+++ src/workflow_docs/fetch_docs_sources.sh
@@ -16,22 +16,25 @@ git clone --no-checkout --depth=1 --single-branch --branch=master --filter=tree:
 git clone --no-checkout --depth=1 --single-branch --branch=master --filter=tree:0 https://github.com/microbiomedata/mg_annotation /tmp/clones/mg_annotation         && cd /tmp/clones/mg_annotation     && git sparse-checkout set --no-cone /docs && git checkout
 git clone --no-checkout --depth=1 --single-branch --branch=master --filter=tree:0 https://github.com/microbiomedata/metaMAGs /tmp/clones/metaMAGs                   && cd /tmp/clones/metaMAGs          && git sparse-checkout set --no-cone /docs && git checkout
 git clone --no-checkout --depth=1 --single-branch --branch=main   --filter=tree:0 https://github.com/microbiomedata/metaT /tmp/clones/metaT                         && cd /tmp/clones/metaT             && git sparse-checkout set --no-cone /docs && git checkout
-git clone --no-checkout --depth=1 --single-branch --branch=main   --filter=tree:0 https://github.com/microbiomedata/metaT_ReadsQC /tmp/clones/metaT_ReadsQC
-git clone --no-checkout --depth=1 --single-branch --branch=main   --filter=tree:0 https://github.com/microbiomedata/metaT_Assembly /tmp/clones/metaT_Assembly
-git clone --no-checkout --depth=1 --single-branch --branch=main   --filter=tree:0 https://github.com/microbiomedata/metaT_ReadCounts /tmp/clones/metaT_ReadCounts
+git clone --no-checkout --depth=1 --single-branch --branch=main   --filter=tree:0 https://github.com/microbiomedata/metaT_ReadsQC /tmp/clones/metaT_ReadsQC         && cd /tmp/clones/metaT_ReadsQC     && git sparse-checkout set --no-cone /docs && git checkout
+git clone --no-checkout --depth=1 --single-branch --branch=main   --filter=tree:0 https://github.com/microbiomedata/metaT_Assembly /tmp/clones/metaT_Assembly       && cd /tmp/clones/metaT_Assembly    && git sparse-checkout set --no-cone /docs && git checkout
+git clone --no-checkout --depth=1 --single-branch --branch=master --filter=tree:0 https://github.com/microbiomedata/metaT_ReadCounts /tmp/clones/metaT_ReadCounts   && cd /tmp/clones/metaT_ReadCounts  && git sparse-checkout set --no-cone /docs && git checkout
 git clone --no-checkout --depth=1 --single-branch --branch=master --filter=tree:0 https://github.com/microbiomedata/metaPro /tmp/clones/metaPro                     && cd /tmp/clones/metaPro           && git sparse-checkout set --no-cone /docs && git checkout
 git clone --no-checkout --depth=1 --single-branch --branch=master --filter=tree:0 https://github.com/microbiomedata/metaMS /tmp/clones/metaMS                       && cd /tmp/clones/metaMS            && git sparse-checkout set --no-cone /docs && git checkout
 git clone --no-checkout --depth=1 --single-branch --branch=master --filter=tree:0 https://github.com/microbiomedata/enviroMS /tmp/clones/enviroMS                   && cd /tmp/clones/enviroMS          && git sparse-checkout set --no-cone /docs && git checkout
 
 # Arrange the "docs" directories into chapters.
 mkdir -p /tmp/book/src/chapters
-#placeholder for metaG end to end chapter which will come from this repo
-#                                      /tmp/book/src/chapters/1_Metagenome Workflow Overview
-mv /tmp/clones/ReadbasedAnalysis/docs /tmp/book/src/chapters/2_Read_Based_Taxonomy 
-mv /tmp/clones/ReadsQC/docs           /tmp/book/src/chapters/3_Metagenome_Reads_QC 
+#
+# TODO: Include Chater 1, the end-to-end MetaG chapter.
+#       Its source files will reside in this `docs` Git repository.
+#       Its chapter slug will be `1_Metagenome_Workflow_Overview`.
+#
+mv /tmp/clones/ReadbasedAnalysis/docs /tmp/book/src/chapters/2_Read_Based_Taxonomy
+mv /tmp/clones/ReadsQC/docs           /tmp/book/src/chapters/3_Metagenome_Reads_QC
 mv /tmp/clones/metaAssembly/docs      /tmp/book/src/chapters/4_Metagenome_Assembly
 mv /tmp/clones/mg_annotation/docs     /tmp/book/src/chapters/5_Metagenome_and_Metatranscriptome_Annotation
-mv /tmp/clones/metaMAGs/docs          /tmp/book/src/chapters/6_Metagenome_Assembled_Genome 
+mv /tmp/clones/metaMAGs/docs          /tmp/book/src/chapters/6_Metagenome_Assembled_Genome
 mv /tmp/clones/metaT/docs             /tmp/book/src/chapters/7_Metatranscriptome_Workflow_Overview
 mv /tmp/clones/metaT_ReadsQC/docs     /tmp/book/src/chapters/8_Metatranscriptome_Reads_QC
 mv /tmp/clones/metaT_Assembly/docs    /tmp/book/src/chapters/9_Metranscriptome_Assembly
--- src/workflow_docs/index.rst
+++ src/workflow_docs/index.rst
@@ -5,22 +5,26 @@
 Welcome to the NMDC Workflow documentation!
 ==========================================
 
+..
+    TODO: Include Chapter 1, the end-to-end MetaG chapter.
+          Its source files will reside in this `docs` Git repository.
+          Its chapter slug will be `1_Metagenome_Workflow_Overview`.
+
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
    chapters/overview.rst
-   chapters/1_Metagenome Workflow Overview/index.rst
-   chapters/2_Read Based Taxonomy/index.rst
-   chapters/3_Metagenome Reads QC/index.rst
-   chapters/4_Metagenome Assembly/index.rst
-   chapters/5_Metagenome and Metatranscriptome Annotation/index.rst
-   chapters/6_Metagenome Assembled Genome/index.rst
-   chapters/7_Metatranscriptome Workflow Overview/index.rst
-   chapters/8_Metatranscriptome Reads QC/index.rst
-   chapters/9_Metranscriptome Assembly/index.rst
-   chapters/10_Metranscriptome Expression/index.rst
+
+   chapters/2_Read_Based_Taxonomy/index.rst
+   chapters/3_Metagenome_Reads_QC/index.rst
+   chapters/4_Metagenome_Assembly/index.rst
+   chapters/5_Metagenome_and_Metatranscriptome_Annotation/index.rst
+   chapters/6_Metagenome_Assembled_Genome/index.rst
+   chapters/7_Metatranscriptome_Workflow_Overview/index.rst
+   chapters/8_Metatranscriptome_Reads_QC/index.rst
+   chapters/9_Metranscriptome_Assembly/index.rst
+   chapters/10_Metranscriptome_Expression/index.rst
    chapters/11_Metaproteomics/index.rst
    chapters/12_Metabolomics/index.rst
-   chapters/13_Natural Organic Matter/index.rst
-
+   chapters/13_Natural_Organic_Matter/index.rst
