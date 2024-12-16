173a16f3df5fa05c52d5650bd54f52fa5d212501
eecavanna <eecavanna@users.noreply.github.com>
2024-12-16 15:47:26 -0800

    Standardize copyright message throughout website

--- content/home/src/conf.py
+++ content/home/src/conf.py
@@ -19,8 +19,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'NMDC Documentation'
-copyright = '2024, The NMDC Team'
-author = 'The NMDC Team'
+copyright = '2024, National Microbiome Data Collaborative'
+author = 'National Microbiome Data Collaborative'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1'
